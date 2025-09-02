c868710bec1376556eaecb3bf1aae58bd9a6289f
Anthony Stainton <anthonystainton+github@gmail.com>
2025-09-01 23:42:46 -0700

    untested let hope it works lol

--- ExampleMod/Patches.cs
+++ ExampleMod/Patches.cs
@@ -4,11 +4,11 @@ using BoneProjectionLib;
 
 namespace PeterBrokenWorlds;
 // bound to this because it will always be on the player when they spawn.
-[HarmonyPatch(typeof(PlayerAnimationHandler))]
+[HarmonyPatch(typeof(PlayerSkinSetter))]
 public class PlayerPatches {
-    [HarmonyPatch(nameof(PlayerAnimationHandler.Awake))]
+    [HarmonyPatch(nameof(PlayerSkinSetter.FinishSkinSwap))]
     [HarmonyPostfix]
-    private static void AwakePostFix(PlayerAnimationHandler __instance) {
+    private static void FinishSkinSwap(PlayerSkinSetter __instance) {
         if (!BoneProjectionModelSwap.Enabled) return;
         // Setup Clone
         GameObject clone = GameObject.Instantiate(BoneProjectionModelSwap.prefab);
@@ -18,8 +18,8 @@ public class PlayerPatches {
         clone.transform.localEulerAngles = Vector3.zero;
 
         // Setup Zoe Model
-        GameObject zoeModel = __instance.gameObject.transform.Find("Visual/Courier_Retake").gameObject;
-        zoeModel.transform.Find("Courier/Meshes").gameObject.SetActive(false);
+        GameObject zoeModel = __instance.gameObject;
+        zoeModel.transform.Find("Courier/Armature").gameObject.SetActive(false);
         zoeModel.transform.localScale = Vector3.one * BoneProjectionModelSwap.zoeScale;
 
         // Do the Projection
@@ -27,10 +27,9 @@ public class PlayerPatches {
         boneProjector.armsHeight = BoneProjectionModelSwap.armHeight;
         boneProjector.armsDistance = BoneProjectionModelSwap.armDistance;
         boneProjector.TargetMixamo(clone.transform.Find("Griffin"));
-        boneProjector.SourceZoe(__instance.transform);
+        boneProjector.SourceZoe(__instance.currentHip.transform);
         boneProjector.source.shoulderL = null; // Shoulders don't look too pretty on peter so we'll remove them
         boneProjector.source.shoulderR = null; 
-        boneProjector.source.hips.gameObject.SetActive(false); // To hide her bags and stuff, Everything still works.
         boneProjector.Setup();
         BoneProjectionModelSwap.currentProjector = boneProjector;
         BoneProjectionModelSwap.curZoe = zoeModel;
--- SonicModelSwap/Patches.cs
+++ SonicModelSwap/Patches.cs
@@ -4,11 +4,11 @@ using BoneProjectionLib;
 
 namespace SonicModel;
 // bound to this because it will always be on the player when they spawn.
-[HarmonyPatch(typeof(PlayerAnimationHandler))]
+[HarmonyPatch(typeof(PlayerSkinSetter))]
 public class PlayerPatches {
-    [HarmonyPatch(nameof(PlayerAnimationHandler.Awake))]
+    [HarmonyPatch(nameof(PlayerSkinSetter.FinishSkinSwap))]
     [HarmonyPostfix]
-    private static void AwakePostFix(PlayerAnimationHandler __instance) {
+    private static void FinishSkinSwap(PlayerSkinSetter __instance) {
         if (!BoneProjectionModelSwap.Enabled) return;
         // Setup Clone
         GameObject clone = GameObject.Instantiate(BoneProjectionModelSwap.prefab);
@@ -18,8 +18,8 @@ public class PlayerPatches {
         clone.transform.localEulerAngles = Vector3.zero;
 
         // Setup Zoe Model
-        GameObject zoeModel = __instance.gameObject.transform.Find("Visual/Courier_Retake").gameObject;
-        zoeModel.transform.Find("Courier/Meshes").gameObject.SetActive(false);
+        GameObject zoeModel = __instance.gameObject;
+        zoeModel.transform.Find("Courier/Armature").gameObject.SetActive(false);
         zoeModel.transform.localScale = Vector3.one * BoneProjectionModelSwap.zoeScale;
 
         // Do the Projection
@@ -27,7 +27,7 @@ public class PlayerPatches {
         boneProjector.armsHeight = BoneProjectionModelSwap.armHeight;
         boneProjector.armsDistance = BoneProjectionModelSwap.armDistance;
         boneProjector.TargetMixamo(clone.transform.Find("Sonic"));
-        boneProjector.SourceZoe(__instance.transform);
+        boneProjector.SourceZoe(__instance.currentHip.transform);
         boneProjector.Setup();
         BoneProjectionModelSwap.currentProjector = boneProjector;
         BoneProjectionModelSwap.curZoe = zoeModel;
